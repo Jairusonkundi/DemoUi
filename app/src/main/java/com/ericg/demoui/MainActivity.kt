1a800b0ba648e6fd106bd30b8cecad0110bb77a9
Ericgacoki <gacokieric@gmail.com>
2021-02-09 16:16:40 +0300

    Implemented DataBinding

--- app/src/main/java/com/ericg/demoui/MainActivity.kt
+++ app/src/main/java/com/ericg/demoui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ericg.demoui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ericg.demoui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
