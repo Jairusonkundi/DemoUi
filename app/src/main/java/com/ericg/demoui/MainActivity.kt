c2d270ca6aa7d8cb481c1e2b541b594942d8b2ab
Ericgacoki <gacokieric@gmail.com>
2021-02-10 16:01:36 +0300

    created a function

--- app/src/main/java/com/ericg/demoui/MainActivity.kt
+++ app/src/main/java/com/ericg/demoui/MainActivity.kt
@@ -9,6 +9,20 @@ class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        handleActions()
     }
+
+    private fun handleActions(){
+        binding?.btnLogIn?.setOnClickListener{
+            val inputs = arrayOf(binding?.regNo, binding?.password)
+            inputs.forEach { input ->
+                if (input!!.text.toString().isEmpty()){
+                    input.error = "required"
+                }
+            }
+        }
+    }
+
 }
